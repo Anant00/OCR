b28f1fa4571b395698c85a8eabc4d58cf07f2283
Anant00 <anantpundir02@gmail.com>
2019-11-20 12:35:18 +0530

    optimise

--- app/src/main/java/com/app/ocr/repository/OCR.kt
+++ app/src/main/java/com/app/ocr/repository/OCR.kt
@@ -1,14 +1,57 @@
 package com.app.ocr.repository
 
 import android.app.Activity
+import android.graphics.Bitmap
+import androidx.annotation.WorkerThread
+import com.app.ocr.utils.Details
+import com.app.ocr.utils.RegexPatterns
+import com.app.ocr.utils.showLog
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 
 object OCR {
+    private var phoneNumber: String? = null
+    private var email: String? = null
+    private var blockText: String? = null
 
     fun pickImageOrUseCamera(context: Activity){
         CropImage.activity()
             .setGuidelines(CropImageView.Guidelines.ON)
             .start(context)
+    }
+
+    @WorkerThread
+    fun recognizeText(result: FirebaseVisionText?, image: Bitmap?): Details {
+        if (result == null || image == null) {
+            showLog("could not recognise image")
+        } else {
+            blockText = result.text
+            for (block in result.textBlocks) {
+                for (line in block.lines) {
+                    val lineText = line.text
+                    if (RegexPatterns.phonePattern.matcher(lineText).matches()) {
+                        phoneNumber = lineText
+                    } else if (RegexPatterns.usPhonePattern.matcher(lineText).matches()) {
+                        phoneNumber = lineText
+                    }
+                    if (RegexPatterns.emailPattern.matcher(lineText).matches()) {
+                        email = lineText
+                    }
+                    for (element in line.elements) {
+                        if (RegexPatterns.phonePattern.matcher(lineText).matches()) {
+                            phoneNumber = lineText
+                        } else if (RegexPatterns.usPhonePattern.matcher(lineText).matches()) {
+                            phoneNumber = lineText
+                        }
+                        if (RegexPatterns.emailPattern.matcher(lineText).matches()) {
+                            email = lineText
+                        }
+                    }
+                }
+            }
+        }
+        showLog("detail  phone is  $phoneNumber")
+        return Details(email, phoneNumber, blockText)
     }
 }
--- app/src/main/java/com/app/ocr/view/MainActivity.kt
+++ app/src/main/java/com/app/ocr/view/MainActivity.kt
@@ -6,34 +6,28 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore.Images.Media.getBitmap
-import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import androidx.annotation.WorkerThread
 import androidx.appcompat.app.AppCompatActivity
 import com.app.ocr.R
 import com.app.ocr.repository.OCR
-import com.app.ocr.utils.Details
-import com.app.ocr.utils.RegexPatterns.emailPattern
-import com.app.ocr.utils.RegexPatterns.phonePattern
-import com.app.ocr.utils.RegexPatterns.usPhonePattern
+import com.app.ocr.repository.OCR.recognizeText
 import com.app.ocr.utils.showLog
 import com.app.ocr.utils.showToast
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
-import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     companion object{
-        private val tag by lazy{
-            MainActivity::class.java.simpleName
-        }
         private var phoneNumber: String? = null
         private var email: String? = null
         private var blockText: String? = null
@@ -91,48 +85,11 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    @WorkerThread
-    private fun recognizeText(result: FirebaseVisionText?, image: Bitmap?): Details {
-        if (result == null || image == null) {
-          showLog("could not recognise image")
-        }
-        else {
-            showLog("result Text: ${result.text} ")
-            blockText = result.text
-            for (block in result.textBlocks) {
-                Log.d(tag, "block text is :  $blockText")
-                for (line in block.lines) {
-                    val lineText = line.text
-                    if (phonePattern.matcher(lineText).matches()){
-                        phoneNumber = lineText
-                    }else if (usPhonePattern.matcher(lineText).matches()){
-                        phoneNumber = lineText
-                    }
-                    if (emailPattern.matcher(lineText).matches()){
-                        email= lineText
-                    }
-                    for (element in line.elements) {
-                        if (phonePattern.matcher(lineText).matches()){
-                            phoneNumber = lineText
-                            Log.d(tag, "phone is : $lineText ")
-                        }
-                        else if (usPhonePattern.matcher(lineText).matches()){
-                            phoneNumber = lineText
-                        }
-                        if (emailPattern.matcher(lineText).matches()){
-                            email= lineText
-                        }
-                    }
-                }
-            }
-        }
-        showLog("phone Number: $phoneNumber, email : $email, block: $blockText ")
-        return Details(email, phoneNumber, blockText)
-    }
+
 
     @SuppressLint("SetTextI18n")
     private fun setData(email: String?, phone: String?, result: String?){
-        if (phoneNumber == null){
+        if (phone == null) {
             tvPhone.text = "Sorry, could not get the phone number, make sure image is clear and try again"
         }else{
             tvPhone.text = "Phone:\n $phone"
